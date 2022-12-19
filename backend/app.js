88faae794d463c2a5f67efc013cc8a334506ded0
Leemyeongjun <2myeongjun@naver.com>
2022-12-19 12:06:53 +0900

    수정

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,42 @@
 const express = require('express');
+
 const request = require('request');
 
-const port = 3000;
+const port = 3000; // 웹서버 포트 번호
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
-app.get('/weather', (req, res) => {  
+app.get('/weather', (req, res) => {   
 
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Content-Type', 'application/json; charset=UTF-8');
 
-  let openApiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?" +
+  let openApiUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?" +
   "serviceKey=gYtciC7e5aLRxDH%2F37pIjbbu0gDYmKAKHcKOxNavvk03LeU5SrNJ6KTBlhHdbwNW5X0jNvvul4i3xo96OkMAGw%3D%3D" +
   "&pageNo=1" +
-  "&numOfRows=1000" +
+  "&numOfRows=5000" +
   "&dataType=JSON" +
   "&base_date=" + req.query.newDate +
   "&base_time=" + req.query.baseTime +
   "&nx=60" +
   "&ny=127";
 
+  console.log(openApiUrl);
+
   request.get({
     uri: openApiUrl
   }, (error, reponse, body) =>{
-    // res.send(JSON.stringify(body));
     res.send(body);
+    // console.log(body);
   });
 });
 
-
+// 웹서버 실행하기
 app.listen(
-  3000,                          
-  () => {                        
+  3000,                          // 포트 번호 지정
+  () => {                        // 서버가 시작 되었을 때 호출될 메서드
     console.log(`${port}번 포트에서 서버 시작했음!`);
   }
 );
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,13 +1,17 @@
+let back = document.querySelector('.back');
 let weatherImg = document.querySelector('.weatherImg');
 let weatherText = document.querySelector('.weatherText');
 let wind = document.querySelector('.wind');
 let windText = document.querySelector('.windText');
 let humidity = document.querySelector('.humidity');
 let precipitation = document.querySelector('.precipitation');
+let tmx = document.querySelector('.tmx');
+let tmn = document.querySelector('.tmn');
+let clockText = document.querySelector('.clockText');
 
 let time = document.querySelector('.titleTime');
 
-let clockText = document.querySelector('.clockText');
+let footer = document.querySelector('.footer');
 
 let info;
 
@@ -19,24 +23,24 @@ let yyyy = 0,
     seconds = 0,
     newDate = 0;
 
-(function() {
+(function () {
   let xhr = new XMLHttpRequest();
 
   let date = new Date();
 
   yyyy = date.getFullYear();
   mm = date.getMonth() + 1;
-  dd = date.getDate();
+  dd = date.getDate() ;
 
   hours = date.getHours();
   minutes = date.getMinutes();
   seconds = date.getSeconds();
 
-  newDate = `${yyyy}${mm}${dd}`;
-  console.log(newDate);
+  let baseTime = 0;
+
   time.innerHTML = yyyy + '-' + mm + '-' + dd;
 
-  let baseTime;
+  newDate = `${yyyy}${mm}${dd}`;
 
   if(hours >= 2 && hours < 5) {
     baseTime = '0200';
@@ -44,7 +48,7 @@ let yyyy = 0,
     baseTime = '0500';
   } else if (hours >= 8 && hours < 11) {
     baseTime = '0800';
-  } else if (hours >= 11 && hours < 4) {
+  } else if (hours >= 11 && hours < 14) {
     baseTime = '1100';
   } else if (hours >= 14 && hours < 17) {
     baseTime = '1400';
@@ -52,24 +56,35 @@ let yyyy = 0,
     baseTime = '1700';
   } else if (hours >= 20 && hours < 23) {
     baseTime = '2000';
-  } else if (hours >=23 && hours < 24) {
+  } else if (hours >= 23 && hours < 24) {
     baseTime = '2300';
   }
-
+  console.log(hours);
+  console.log(newDate);
+  console.log(baseTime);
   xhr.open(
-    "GET", 
+    "GET",
     "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?" +
-    "serviceKey=gYtciC7e5aLRxDH%2F37pIjbbu0gDYmKAKHcKOxNavvk03LeU5SrNJ6KTBlhHdbwNW5X0jNvvul4i3xo96OkMAGw%3D%3D" +
+    "serviceKey=s2eMCzdkGrxPMhVr%2Bhv2U3bF%2Bj3ui%2B7qUS4r0tJKms0%2Bm%2FDN2TCj76L0o%2BacXVPei8K2lxKVFICQraz0iZgJ5g%3D%3D" +
     "&pageNo=1" +
-    "&numOfRows=1000" +
+    "&numOfRows=5000" +
     "&dataType=JSON" +
     "&base_date=" + newDate +
     "&base_time=" + baseTime +
-    "&nx=60" +
-    "&ny=127", false);
+    "&nx=98" +
+    "&ny=76", false);
   xhr.send();
 
+  // 서울 60 127
+  // 부산 98 76
+  // 대구 89 90
+  // 인천 55 124
+  // 광주 58 74
+  // 대전 67 100
+
   info = JSON.parse(xhr.responseText);
+
+  console.log(info);
 
   let tmp = 0,
       reh = 0,
@@ -77,68 +92,86 @@ let yyyy = 0,
       pty = 0,
       sky = 0,
       pop = 0;
+      tmx = 0;
+      tmn = 0;
+
 
   info.response.body.items.item.forEach((index) => {
-    if(index.category == 'TMP') {
+    if (index.category == 'TMP') {
       tmp = parseInt(index.fcstValue);
-    } else if(index.category == 'REH') {
+    } else if (index.category == 'REH') {
       reh = parseInt(index.fcstValue);
-    } else if(index.category == 'WSD') {
+    } else if (index.category == 'WSD') {
       wsd = index.fcstValue;
-    } else if(index.category == 'PTY') {
+    } else if (index.category == 'PTY') {
       pty = parseInt(index.fcstValue);
-    } else if(index.category == 'SKY') {
+    } else if (index.category == 'SKY') {
       sky = parseInt(index.fcstValue);
-    } else if(index.category == 'POP') {
+    } else if (index.category == 'POP') {
       pop = parseInt(index.fcstValue);
+    } else if (index.category == 'TMX') {
+      tmx = parseInt(index.fcstValue);
+    } else if (index.category == 'TMN') {
+      tmn = parseInt(index.fcstValue);
     }
+  
   });
 
-  console.log(tmp);
 
   document.querySelector('.weatherTmp').innerHTML = `${tmp}°`;
+  document.querySelector('.tmx').innerHTML = `↑${tmx}°`;
+  document.querySelector('.tmn').innerHTML = `↓${tmn}°`;
 
-  if(pty == 0 && sky == 1) {
+  if (pty == 0 && sky == 1) {
     weatherImg.style = 'background-image: url(../frontend/img/sun.png)';
+    back.style = 'background-image: url(../frontend/img/back1.jpg)';
     weatherText.innerHTML = '맑음';
-  } else if(pty == 0 && sky == 3) {
+  } else if (pty == 0 && sky == 3) {
     weatherImg.style = 'background-image: url(../frontend/img/cloud.png)';
+    back.style = 'background-image: url(../frontend/img/back1.jpg)';
     weatherText.innerHTML = '구름';
-  } else if(pty == 0 && sky == 4) {
+  } else if (pty == 0 && sky == 4) {
     weatherImg.style = 'background-image: url(../frontend/img/blackcloud.png)';
+    back.style = 'background-image: url(../frontend/img/back2.jpg)';
     weatherText.innerHTML = '흐림';
-  } else if(pty == 1) {
+  } else if (pty == 1) {
     weatherImg.style = 'background-image: url(../frontend/img/rain.png)';
+    back.style = 'background-image: url(../frontend/img/back2.jpg)';
     weatherText.innerHTML = '비';
-  } else if(pty == 2) {
+  } else if (pty == 2) {
     weatherImg.style = 'background-image: url(../frontend/img/rainsnow.png)';
+    back.style = 'background-image: url(../frontend/img/back3.jpg)';
     weatherText.innerHTML = '비와 눈';
-  } else if(pty == 3) {
+  } else if (pty == 3) {
     weatherImg.style = 'background-image: url(../frontend/img/snow.png)';
+    back.style = 'background-image: url(../frontend/img/back3.jpg)';
     weatherText.innerHTML = '눈';
-  } else if(pty == 4) {
+  } else if (pty == 4) {
     weatherImg.style = 'background-image: url(../frontend/img/rainrain.png)';
+    back.style = 'background-image: url(../frontend/img/back2.jpg)';
     weatherText.innerHTML = '소나기';
   }
 
-  if(wsd >= 0 && wsd < 1) {
+  if (wsd >= 0 && wsd < 1) {
     wind.innerHTML = '고요';
-  } else if(wsd >= 1 && wsd < 2) {
+  } else if (wsd >= 1 && wsd < 2) {
     wind.innerHTML = '아주약함';
-  } else if(wsd >= 2 && wsd < 3) {
+  } else if (wsd >= 2 && wsd < 3) {
     wind.innerHTML = '약함';
-  } else if(wsd >= 3 && wsd < 5) {
+  } else if (wsd >= 3 && wsd < 5) {
     wind.innerHTML = '중간';
-  } else if(wsd >= 5 && wsd < 7) {
+  } else if (wsd >= 5 && wsd < 7) {
     wind.innerHTML = '조금강함';
-  } else if(wsd >= 7 && wsd < 10) {
+  } else if (wsd >= 7 && wsd < 10) {
     wind.innerHTML = '강함';
-  } else if(wsd >= 10 && wsd < 12) {
+  } else if (wsd >= 10 && wsd < 12) {
     wind.innerHTML = '태풍';
   }
 
-  wind.style = 'background-image: url(../frontend/img/wind.png)';
 
+
+  wind.style = 'background-image: url(../frontend/img/wind.png)';
+  
   humidity.style = 'background-image: url(../frontend/img/humidity.png)';
   humidity.innerHTML = `${reh}%`;
 
@@ -161,11 +194,9 @@ function clock() {
   clockText.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes }`  : minutes }:${seconds < 10 ? `0${seconds }`  : seconds }`;
 }
 
-function init() {
+(function init() {
 
   clock();
   
   setInterval(clock, 1000);
-}
-  
-init();
+})();
